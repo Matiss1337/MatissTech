338be1a3ce642d8b6c76e2fa281899e23f61d503
Matiss1337 <matiss1337@gmail.com>
2024-03-24 19:59:04 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         'primary': '#ff5152',
+        'secondary': '#fcfdf5',
+        'accent': '#141414'
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+      borderRadius: {
+        'normal': '4px',
       },
     },
   },
